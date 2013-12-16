d83428d39453ee6ccfb75332c45b23ddec7b3d11
Luwangel <adrien.amoros@gmail.com>
2013-12-16 19:55:14 +0100

    Création d'un nouveau projet propre (problèmes de compatibilité)
    Modification de WebServiceConstants

--- VirtualUT/build/source/buildConfig/debug/fr/if26/virtualut/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package fr.if26.virtualut;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
