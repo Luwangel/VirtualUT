5ced0959b177b430b30165aae58fd087d0b9ad13
Thanh-Tuan TRINH <thanh_tuan.trinh@utt.fr>
2013-12-26 21:16:51 +0100

    Ajout page compte:
    - fragment imbriqué (nested fragment) pour la liste des transactions
    - boutons trier date/montant
    - date du solde fonctionnel (Time.setToNow)
    - textview qui défile automatiquement si libellé trop long (marquee)
    
    Signed-off-by: Thanh-Tuan TRINH <thanh_tuan.trinh@utt.fr>

---
+++ VirtualUT/VirtualUT/src/main/java/fr/if26/virtualut/model/Transaction.java
@@ -0,0 +1,14 @@
+package fr.if26.virtualut.model;
+
+/**
+ * Created by Administrateur on 22/12/13.
+ */
+public class Transaction {
+
+    public int idSender;
+    public int idReceiver;
+    public String date;
+    public int montant;
+    public String libelle;
+
+}
