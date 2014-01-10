58f66b58f1419c348159d43f634beabaa91f0ac1
Luwangel <adrien.amoros@gmail.com>
2014-01-10 21:35:24 +0100

    Mise à jour serveur : correction du transfert

--- Serveur/transfertByMembre.php
+++ Serveur/transfertByMembre.php
@@ -41,40 +41,32 @@
 		//Si le virement est effectif
 		if ($parameters[":valide"] == 1 ) {
 
-			$credit=$user->credit;
+			$creditSender=$user->credit;
 			
 			//Vérifie le montant
-			if($montant <= $credit and $credit > 0) {
+			if($montant <= $creditSender and $creditSender > 0) {
 				
 				//Calcule le nouveau crédit
-				$credit -= $montant;
+				$creditSender -= $montant;
 
 				//Met à jour le crédit du sender
-				$success = $db->updateMembre($credit,$user->idMembre);
+				$db->updateMembre($creditSender,$user->idMembre);
 			
-				if($success == 1) {
-					//Récupère le receiver
-					$receiver = $db->getMembreById($parameters[':idReceiver']);
+				//Récupère le receiver
+				$receiver = $db->getMembreById($parameters[':idReceiver']);
 					
-					//Calcule le nouveau crédit
-					$credit = $receiver->credit;
-					$credit += $montant;
+				//Calcule le nouveau crédit
+				$creditReceiver = $receiver->credit;
+				$creditReceiver += $montant;
 
-					//Met à jour le receiver
-					$receiver = $db->updateMembre($credit,$parameters[':idReceiver']);
+				//Met à jour le receiver
+				$receiver = $db->updateMembre($creditReceiver,$parameters[':idReceiver']);
 					
-					//Ajoute une ligne à la table compte
-					$id = $db->insertCompte($user->idMembre,$parameters[':idReceiver'],$datesend,$unite,$parameters[':libelle'],1);
+				//Ajoute une ligne à la table compte
+				$db->insertCompte($user->idMembre,$parameters[':idReceiver'],$datesend,$montant,$parameters[':libelle'],1);
 					
-					//Déclare qu'il n'y a pas eu d'erreur
-					$json = array('error' => false);
-				}
-				else {
-					$json = array(
-						'error' => true,
-						'message'=> "Erreur, transaction annulée."
-					);
-				}
+				//Déclare qu'il n'y a pas eu d'erreur
+				$json = array('error' => false);
 			}
 			else {
 				$json = array(
